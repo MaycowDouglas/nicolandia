0839f70d2a4485729a98fc9c38326e16550a1d4c
Maycow Marques <melo.maycow@gmail.com>
2023-05-03 11:33:59 -0300

    change main banner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,8 @@ import CTANicoApp from '@/components/CTA/NicoApp'
 import Layout from '@/components/Layout'
 import MainBanner from '@/components/MainBanner'
 import PassaportCarousel from '@/components/Passaport/PassaportCarousel'
+import BannerOneMobile from '@/public/images/banners/one-mobile.png'
+import BannerOneWeb from '@/public/images/banners/one-web.png'
 import SwiperCore, { Autoplay } from 'swiper'
 import { Swiper, SwiperSlide } from 'swiper/react'
 SwiperCore.use([Autoplay])
@@ -20,15 +22,8 @@ export default function HomePage() {
           <SwiperSlide>
             <MainBanner
               alt="Diversão para toda a Família!"
-              mobile="https://yuupe.com/uploads/552aa143a150078fc62fbd3f1e640cfb.jpeg"
-              desktop="https://yuupe.com/uploads/23e84158358d248dfc85554ab86e2352.jpeg"
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <MainBanner
-              alt="Passaporte Sessão Extra, por R$ 50,00 - Todo sábado e domingo a partir das 17h na bilheteria presencial."
-              mobile="https://yuupe.com/uploads/4ce6855a3e316aba22559fdc99bca6ae.jpeg"
-              desktop="https://yuupe.com/uploads/2b0f16217555626ecd9998ce6e70a8a6.jpeg"
+              mobile={BannerOneMobile}
+              desktop={BannerOneWeb}
             />
           </SwiperSlide>
         </Swiper>
