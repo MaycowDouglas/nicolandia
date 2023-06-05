164360114c6f059ccfe2204f6e219c6b0b2706ba
Maycow Marques <melo.maycow@gmail.com>
2023-06-05 20:08:51 -0300

    add promotional banner and feature of main banner with video and image

--- src/components/MainBanner.tsx
+++ src/components/MainBanner.tsx
@@ -2,60 +2,127 @@ import Image, { StaticImageData } from 'next/image'
 import { MutableRefObject, VideoHTMLAttributes, useEffect, useRef, useState } from 'react'
 import { FaPlay } from 'react-icons/fa'
 
+import PlayButton from './PlayButton'
+
 type MainBannerProps = {
-  alt: string
+  alt?: string
+  mobile: string | StaticImageData
+  desktop: string | StaticImageData
+  isVideo?: boolean
+  onPlay?: () => void
+  onPause?: () => void
+}
+
+type SlideVideoProps = {
+  mobile: string
+  desktop: string
+  onPlay: () => void
+  onPause: () => void
+}
+
+type SlideImageProps = {
+  alt?: string
   mobile: string | StaticImageData
   desktop: string | StaticImageData
 }
 
-export default function MainBanner({ desktop, mobile, alt }: MainBannerProps) {
-  const videoWeb = useRef<any>(null)
-  const videoMobile = useRef<any>(null)
-  const [isPlaying, setIsPlaying] = useState(false)
+export default function MainBanner({
+  alt,
+  mobile,
+  desktop,
+  isVideo,
+  onPlay,
+  onPause,
+}: MainBannerProps) {
+  return isVideo && onPlay instanceof Function && onPause instanceof Function ? (
+    <SlideVideo
+      desktop={desktop as string}
+      mobile={mobile as string}
+      onPlay={onPlay}
+      onPause={onPause}
+    />
+  ) : (
+    <SlideImage desktop={desktop} mobile={mobile} alt={alt} />
+  )
+}
 
-  function handlePlayClick(ref: any) {
-    setIsPlaying(true)
-    ref.current.play()
-  }
+function SlideImage({ desktop, mobile, alt = '' }: SlideImageProps) {
+  const [imageSize, setImageSize] = useState({ width: 0, height: 0 })
+  const [imageSource, setImageSource] = useState<string | StaticImageData>('')
+
+  useEffect(() => {
+    const handleResize = () => {
+      const screenWidth = window.innerWidth
+      setImageSize(screenWidth < 768 ? { width: 415, height: 900 } : { width: 1920, height: 950 })
+      setImageSource(screenWidth < 768 ? mobile : desktop)
+    }
+    handleResize()
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [desktop, mobile])
 
   return (
     <div className="relative w-full bg-red">
+      <Image
+        alt={alt}
+        src={imageSource}
+        width={imageSize.width}
+        height={imageSize.height}
+        className="w-full h-auto"
+      />
+    </div>
+  )
+}
+
+function SlideVideo({ mobile, desktop, onPlay, onPause }: SlideVideoProps) {
+  const videoRef = useRef<any>(null)
+  const [isPlaying, setIsPlaying] = useState(false)
+  const [videoSource, setVideoSource] = useState<string>('')
+  const [playbackTime, setPlaybackTime] = useState(0)
+
+  useEffect(() => {
+    const handleResize = () => {
+      const screenWidth = window.innerWidth
+      setVideoSource(screenWidth < 768 ? mobile : desktop)
+    }
+    handleResize()
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [desktop, mobile])
+
+  function handlePlayClick() {
+    onPlay()
+    setIsPlaying(true)
+    videoRef.current.play()
+  }
+
+  function handlePause() {
+    onPause()
+    setIsPlaying(false)
+    videoRef.current.currentTime = 0
+  }
+
+  return (
+    <div className="relative">
       <div
         className={
           isPlaying ? 'hidden' : 'bg-dark bg-opacity-30 absolute top-0 left-0 right-0 bottom-0'
         }
       ></div>
-      <button
-        className={`z-50 w-32 h-32 hidden md:inline-block ${
-          isPlaying ? 'md:hidden' : 'absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'
-        }`}
-        onClick={() => handlePlayClick(videoWeb)}
-      >
-        <span className="absolute z-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 inline-flex w-28 h-28 rounded-full bg-light bg-opacity-30 animate-pulse"></span>
-        <span className="absolute z-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 inline-flex w-20 h-20 rounded-full bg-light bg-opacity-50 animate-pulse"></span>
-        <FaPlay className="absolute z-30 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 inline-flex text-light text-2xl" />
-      </button>
-      <button
-        className={`z-50 w-32 h-32 md:hidden ${
-          isPlaying ? 'hidden' : 'absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'
-        }`}
-        onClick={() => handlePlayClick(videoMobile)}
-      >
-        <span className="absolute z-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 inline-flex w-28 h-28 rounded-full bg-light bg-opacity-30 animate-pulse"></span>
-        <span className="absolute z-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 inline-flex w-20 h-20 rounded-full bg-light bg-opacity-50 animate-pulse"></span>
-        <FaPlay className="absolute z-30 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 inline-flex text-light text-2xl" />
-      </button>
+      <PlayButton show={isPlaying} onClick={handlePlayClick} />
+
       <video
-        loop
-        ref={videoWeb}
-        src="/videos/kabum-web.mp4"
-        className="hidden md:block w-full h-auto"
-      />
-      <video
-        loop
-        ref={videoMobile}
-        src="/videos/kabum-mobile.mp4"
-        className="md:hidden w-full h-auto"
+        ref={videoRef}
+        src={videoSource}
+        className="w-full h-auto"
+        onPause={handlePause}
+        onTimeUpdate={() => setPlaybackTime(videoRef.current.currentTime)}
       />
     </div>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,25 +5,49 @@ import CTANicoApp from '@/components/CTA/NicoApp'
 import Layout from '@/components/Layout'
 import MainBanner from '@/components/MainBanner'
 import PassaportCarousel from '@/components/Passaport/PassaportCarousel'
-import BannerOneMobile from '@/public/images/banners/one-mobile.png'
-import BannerOneWeb from '@/public/images/banners/one-web.png'
+import BannerOneMobile from '@/public/images/banners/namorados-mobile.png'
+import BannerOneWeb from '@/public/images/banners/namorados-web.png'
+import { useRef } from 'react'
 import SwiperCore, { Autoplay } from 'swiper'
 import { Swiper, SwiperSlide } from 'swiper/react'
 SwiperCore.use([Autoplay])
 
 export default function HomePage() {
+  const swiperRef = useRef<any>(null)
+
+  const stopSlideTransition = () => {
+    if (swiperRef.current && swiperRef.current.swiper) {
+      swiperRef.current.swiper.autoplay.stop()
+    }
+  }
+
+  const startSlideTransition = () => {
+    if (swiperRef.current && swiperRef.current.swiper) {
+      swiperRef.current.swiper.autoplay.start()
+    }
+  }
+
   return (
     <Layout
       title="Seu lugar de diversão"
       description="A Nova Nicolândia é um parque de diversões urbano e fechado, garantindo assim a segurança dos visitantes. O mundo da diversão não poderia estar em outro lugar, senão no coração de Brasília. Conheça o nosso parque e apaixone-se!"
     >
       <section>
-        <Swiper autoplay={{ delay: 4000 }}>
+        <Swiper ref={swiperRef} autoplay={{ delay: 4000 }}>
           <SwiperSlide>
             <MainBanner
               alt="Diversão para toda a Família!"
               mobile={BannerOneMobile}
               desktop={BannerOneWeb}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <MainBanner
+              isVideo
+              mobile="/videos/kabum-mobile.mp4"
+              desktop="/videos/kabum-web.mp4"
+              onPlay={stopSlideTransition}
+              onPause={startSlideTransition}
             />
           </SwiperSlide>
         </Swiper>
